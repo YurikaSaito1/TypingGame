0ab989a3a11bd43c0e2674f6ed5fa8693b88372b
YurikaSaito <y.saito.168@gmail.com>
2023-07-10 14:09:20 +0900

    日本語入力（小文字非対応）

--- js/question.js
+++ js/question.js
@@ -1,69 +1,95 @@
 const subject = document.getElementById('subject'); // 問題文
 const input = document.getElementById('input'); // 入力エリア
-//const form = document.getElementById('form'); // 入力フォーム
 const timer = document.getElementById('timer'); // タイマー
+
+const romanMap = {
+    'あ' : ['a'], 'い' : ['i'], 'う' : ['u'], 'え' : ['e'], 'お' : ['o'],
+    'か' : ['ka'], 'き' : ['ki'], 'く' : ['ku'], 'け' : ['ke'], 'こ' : ['ko'],
+    'さ' : ['sa'], 'し' : ['shi', 'si'], 'す' : ['su'], 'せ' : ['se'], 'そ' : ['so'],
+    'た' : ['ta'], 'ち' : ['chi', 'ti'], 'つ' : ['tsu', 'tu'], 'て' : ['te'], 'と' : ['to'],
+    'な' : ['na'], 'に' : ['ni'], 'ぬ' : ['nu'], 'ね' : ['ne'], 'の' : ['no'],
+    'は' : ['ha'], 'ひ' : ['hi'], 'ふ' : ['fu', 'hu'], 'へ' : ['he'], 'ほ' : ['ho'],
+    'ま' : ['ma'], 'み' : ['mi'], 'む' : ['mu'], 'め' : ['me'], 'も' : ['mo'],
+    'や' : ['ya'], 'ゆ' : ['yu'], 'よ' : ['yo'],
+    'ら' : ['ra'], 'り' : ['ri'], 'る' : ['ru'], 'れ' : ['re'], 'ろ' : ['ro'],
+    'わ' : ['wa'], 'を' : ['wo'], 'ん' : ['nn'],
+    'が' : ['ga'], 'ぎ' : ['gi'], 'ぐ' : ['gu'], 'げ' : ['ge'], 'ご' : ['go'],
+    'ざ' : ['za'], 'じ' : ['ji', 'zi'], 'ず' : ['zu'], 'ぜ' : ['ze'], 'ぞ' : ['zo'],
+    'だ' : ['da'], 'ぢ' : ['di'], 'づ' : ['du'], 'で' : ['de'], 'ど' : ['do'],
+    'ば' : ['ba'], 'び' : ['bi'], 'ぶ' : ['bu'], 'べ' : ['be'], 'ぼ' : ['bo'],
+    'ぱ' : ['pa'], 'ぴ' : ['pi'], 'ぷ' : ['pu'], 'ぺ' : ['pe'], 'ぽ' : ['po'],
+    'ー' : '-'
+};
+
 const textList = [
-    'apple',
-    'banana',
-    'orange',
-    'green apple',
-    'strawberry'
+    'りんご',
+    'ばなな',
+    'みかん',
+    'いちご',
+    'ぶどう',
+    'ちくわ',
+    'しそ',
+    'とろろ',
+    'なずな',
+    'いも'
 ];
-let textSplit;
+
+let romanArray = [];
+let text = '';
 
 let count = 0; // 正解数
 let TIME = 20; // 制限時間
 let state = true; // キー入力有効
-let textNum = 0; // 何文字目の入力か
 
 // 開始処理
 function init() {
     const rnd = Math.floor(Math.random() * textList.length);
 
     subject.textContent = textList[rnd];
-    textSplit = subject.textContent.split(''); // 問題文を１文字ずつ格納
-    textNum = 0;
+    text = subject.textContent; // 問題文を格納
+    setChar();
 
     input.textContent = '';
-
-    /*form.input.value = '';
-    form.input.focus();*/
 }
 
 // ゲーム開始
 init();
 
-// 送信ボタンが押されたとき
-/*form.btn.addEventListener('click', function(e) {
-    if(!state) return; // ゲーム終了後は送信できなくする
-    
-    if(form.input.value === subject.textContent) {
-        count++;
-        init();
-    } else {
-        subject.textContent = '間違いです！';
-        setTimeout(function(){ init() },1000)
-    }
-})*/
-
 // キーが押されたとき
 window.addEventListener('keydown', (event) => {
     let key = event.key;
 
-    if(!state) return;
+    if(!state) return;  // ゲーム終了後は操作できなくする
 
-    if (key == textSplit[textNum]) {
-        input.textContent += key;
-        console.log('ok');
-        textNum++;
-    } else {
-        console.log('NG');
-    }
-    if (textNum >= textSplit.length) {
-        count++;
-        setTimeout(function(){ init() },100)
+    // ローマ字の一文字目がヘボン式か訓令式か判定
+    for (let i=0; i<romanArray.length; i++) {
+        if (key == romanArray[i].slice(0, 1)) {
+            input.textContent += romanArray[i].slice(0, 1);
+            for (let j=0; j<romanArray.length; j++) {
+                romanArray[j] = romanArray[j].slice(1);
+            }
+            if (romanArray[i].length == 0) {
+                setChar();
+            }
+        }
     }
 })
+
+// 判定するかな一文字の設定
+function setChar() {
+    let oneChar = text.slice(0, 1); // かな一文字目を取り出す
+    console.log(oneChar);
+    let romanMapArray = JSON.parse(JSON.stringify(romanMap)); // ヘボン式と訓令式をローマ字に変換し、配列に格納
+    romanArray = romanMapArray[oneChar];
+
+    // 文章を入力し終えたら次の問題へ
+    if (text.length == 0) {
+        count++;
+        setTimeout(function(){ init() }, 100);
+    }
+
+    text = text.slice(1); // 一文字削る
+}
 
 // カウントダウン
 const countdown = setInterval(function() {
